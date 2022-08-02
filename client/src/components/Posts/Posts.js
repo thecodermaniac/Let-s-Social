e16b20f1d2b305fb389737c32f2011e3e7805aba
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2022-08-03 01:47:58 +0530

    added post functionality to app

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Post from './Post/Post'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
+import { Grid, CircularProgress } from '@material-ui/core'
 
 function Posts() {
-    const posts= useSelector((state)=> state.posts)
+    const posts = useSelector((state) => state.posts)
     console.log(posts);
     return (
-        <>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+           <Grid>
+            {
+            posts.map((post)=>(
+                <Grid key={post._id} item xs={12} sm={6}>
+                    <Post post={post}/>
+                </Grid>
+            ))
+           }
+           </Grid>
+        )
     )
 }
 
