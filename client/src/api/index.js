addcc84ec1a194f5486d9a991707e54c2c698684
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-01-14 12:48:43 +0530

    made to work with local host

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'https://lets-social-backend.herokuapp.com/posts'
+const url = 'http://localhost:5000/posts'
 
 
 axios.interceptors.request.use((req) => {
@@ -23,7 +23,7 @@ export const likepost = (id) => axios.put(`${url}/like/${id}`)
 
 export const commentPost = (value, id) => axios.post(`${url}/${id}/commentPost`, { value })
 
-const urluser = 'https://lets-social-backend.herokuapp.com/users'
+const urluser = 'http://localhost:5000/users'
 
 export const signIn = (formData) => axios.post(`${urluser}/signin`, formData)
 export const signUp = (formData) => axios.post(`${urluser}/signup`, formData)
