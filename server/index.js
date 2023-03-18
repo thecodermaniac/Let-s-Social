4894a56cf316d59373f4a9f5dd709b6febafebf6
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-03-18 23:52:09 +0530

    all origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const corsOptions = {
 }
 
 const app = express()
-app.use(cors(corsOptions))
+app.use(cors({origin: '*'}))
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 
